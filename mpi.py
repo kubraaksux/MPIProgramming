589100715bacf9753f66e5b3d43087a5bee7fc3b
Kübra Aksu <kubra_4199@hotmail.com>
2023-12-24 18:15:25 +0300

    mpi implementation is done.
    meets the requirements of the project

--- mpi.py
+++ mpi.py
@@ -3,12 +3,14 @@ from mpi4py.futures import MPIPoolExecutor
 import sys
 import numpy as np
 
+
 # Initialize MPI environment
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 status = MPI.Status()
 
+# Define data types and constants
 product_shape = (150,)  # Maximum length of product strings after processing
 product_dtype = MPI.CHAR
 log_size = 30           # Maximum length of maintenance log entries
@@ -19,21 +21,77 @@ operations = {}
 
 # Operations
 def add_operation(products):
+    """
+    Concatenate a list of product strings.
+    
+    Args:
+        products (list): List of product strings.
+    
+    Returns:
+        str: Concatenated product string.
+    """
     return ''.join(products)
 
 def enhance_operation(product):
-    return product[0] + product + product[-1] if product else product
+    """
+    Enhance a product string by adding its first and last characters.
+    
+    Args:
+        product (str): Input product string.
+    
+    Returns:
+        str: Enhanced product string.
+    """
+    if len(product) == 0:
+        return product
+    return product[0] + product + product[-1]
 
 def reverse_operation(product):
+    """
+    Reverse a product string.
+    
+    Args:
+        product (str): Input product string.
+        
+    Returns:
+        str: Reversed product string.
+    """
     return product[::-1]
 
 def chop_operation(product):
+    """
+    Chop off the first and last characters of a product string.
+
+    Args:
+        product (str): Input product string.
+
+    Returns:
+        str: Chopped product string.
+    """
     return product[:-1] if len(product) > 1 else product
 
 def trim_operation(product):
+    """
+    Trim off the first and last characters of a product string.
+    
+    Args:
+        product (str): Input product string.
+
+    Returns:
+        str: Trimmed product string.
+    """
     return product[1:-1] if len(product) > 2 else product
 
 def split_operation(product):
+    """
+    Split a product string into two halves.
+
+    Args:
+        product (str): Input product string.
+    
+    Returns:
+        str: First half of the product string if the length is even, or the first half plus the middle character if the length is odd.
+    """
     mid = len(product) // 2
     return product[:mid] if len(product) % 2 == 0 else product[:mid + 1]
 
@@ -53,6 +111,7 @@ operation_sequence = {
     'even': ['enhance', 'split', 'chop']  # Even machines alternate between enhance, split, and chop
 }
 
+# Parse the input file and extract initial settings
 def parse_input_file(input_file):
     with open(input_file, 'r') as f:
         lines = f.readlines()
@@ -60,65 +119,60 @@ def parse_input_file(input_file):
     num_machines = int(lines[0].strip())
     num_cycles = int(lines[1].strip())
     wear_factors_values = list(map(int, lines[2].strip().split()))
-
-    wear_factors_line = lines[3].split()
-    wear_factors = {
-        'enhance': int(wear_factors_line[0]),
-        'reverse': int(wear_factors_line[1]),
-        'chop': int(wear_factors_line[2]),
-        'trim': int(wear_factors_line[3]),
-        'split': int(wear_factors_line[4]),
-    }
-    threshold = int(lines[4])
-    wear_factors = {op: wear_factors_values[i] for i, op in enumerate(operations) if op != 'add'}
     threshold = int(lines[3].strip())
 
-    parent_child_relations = {i: [] for i in range(1, num_machines + 1)}
-    machine_operations = {}
-    for line in lines[4:4 + num_machines - 1]:
-        child_id, parent_id, *operation = map(int, line.strip().split())
-        parent_child_relations[parent_id].append(child_id)
-        machine_operations[child_id] = operation[0] if operation else 'add'
+    # Initialize the wear factors dictionary for operations
+    operations = ['enhance', 'reverse', 'chop', 'trim', 'split']
+    wear_factors = {op: wear_factors_values[i] for i, op in enumerate(operations)}
 
+    # Initialize dictionaries for parent-child relations and machine operations
+    parent_child_relations = {}
+    machine_operations = {}
+
+    # Parsing the machine operations and parent-child relations
+    for line in lines[4:4 + num_machines]:
+        machine_id, parent_id, operation = line.strip().split()
+        machine_id, parent_id = int(machine_id), int(parent_id)
+
+        if parent_id not in parent_child_relations:
+            parent_child_relations[parent_id] = []
+        parent_child_relations[parent_id].append(machine_id)
+
+        machine_operations[machine_id] = operation
+
+    # Parsing initial products for leaf machines
     initial_products = {}
-    for line in lines[4 + num_machines - 1:]:
+    for line in lines[4 + num_machines:]:
         machine_id, product = line.strip().split(':')
         initial_products[int(machine_id)] = product
 
     return num_cycles, threshold, wear_factors, parent_child_relations, machine_operations, initial_products
 
 
-# Function to be executed on worker nodes, operation functions for factory machines
-def process_data(product):
-    # Data processing logic
-    pass
-
-
-def calculate_maintenance_cost(accumulated_wear, threshold, wear_factor):
+# Calculate the maintenance cost and send the maintenance log to the control room   
+def calculate_maintenance_cost(accumulated_wear, threshold, wear_factor, cycle):
+    maintenance_cost = 0
+    
     if accumulated_wear >= threshold:
-        return (accumulated_wear - threshold + 1) * wear_factor
-    return 0
+        maintenance_cost = (accumulated_wear - threshold + 1) * wear_factor
+        maintenance_message = f"{rank}-{maintenance_cost}-{cycle}"
+
+        # Use MPI.Request for non-blocking send
+        request = comm.isend(maintenance_message, dest=0, tag=maintenance_tag) # Non-blocking send
+        request.wait()
+        
+    return maintenance_cost
+
 
 # Worker process logic for simulating factory machines
-def machine_process(rank, parent, children, operations, wear_factors, threshold, num_cycles, initial_products):
-    current_operation = initial_operation
-    current_operation_index = 0
+def machine_process(rank, parent, children, initial_operation, wear_factors, threshold, num_cycles, initial_products):
     accumulated_wear = 0
-    current_operation = 'add'  # Default initial operation
-    cycle = 0
-    operation_sequence = ['reverse', 'trim'] if rank % 2 else ['enhance', 'split', 'chop']
-
-    is_terminal_machine = parent is None
-    # Determine the sequence of operations for this machine based on its ID
-    operations = operation_sequence['odd' if rank % 2 else 'even']
-    current_operation = operations[current_operation_index]
-
-    # The initial operation for each machine is set by the control room
-    # This information should be received by the worker process
-    # For example, initial_operation = "trim" or "split" etc.
+    global operation_sequence 
+    operation_sequence = operation_sequence['odd' if rank % 2 else 'even']
+    current_operation_index = operation_sequence.index(initial_operation)
 
     for cycle in range(num_cycles):
-        # Receive products from children (if not a leaf machine)
+        # Receive products from children or use initial product if a leaf machine
         received_products = []
         if children:
             for child in children:
@@ -126,98 +180,83 @@ def machine_process(rank, parent, children, operations, wear_factors, threshold,
                 comm.Recv(product, source=child)
                 received_products.append(product.tostring().decode('utf-8'))
         else:
-            received_products = [initial_product]
+            received_products = [initial_products[rank]]
 
-        # Perform operations on the received products
+        # Perform add operation
         processed_product = operations['add'](received_products)
-        if current_operation != 'add':
-            processed_product = operations[current_operation](processed_product)
-            accumulated_wear += wear_factors[current_operation]
 
-        # Perform operations
-        processed_product = add_operation(received_products)
-        if not is_terminal_machine:
-            operation_function = globals()[f"{current_operation}_operation"]
-            processed_product = operation_function(processed_product)
-            accumulated_wear += wear_factors[current_operation]  # Add wears out for non-terminal machines
+        # Perform the current operation if it's not the add operation (for non-terminal machines)
+        if initial_operation != 'add':
+            processed_product = operations[initial_operation](processed_product)
+            accumulated_wear += wear_factors[initial_operation]
 
         # Send processed product to the parent or inform control room if it's the terminal machine
         if parent is not None:
+            # Use blocking `Send` to ensure parent takes the product
             comm.Send(np.array(processed_product, dtype=product_dtype), dest=parent)
         else:
+            # For the terminal machine, send the final product to the control room
             comm.Send(np.array(processed_product, dtype=product_dtype), dest=0, tag=99)
 
         # Check for maintenance
         if accumulated_wear >= threshold:
-            maintenance_cost = calculate_maintenance_cost(accumulated_wear, threshold, wear_factors[current_operation])
+            maintenance_cost = calculate_maintenance_cost(accumulated_wear, threshold, wear_factors[initial_operation])
             maintenance_message = f"{rank}-{maintenance_cost}-{cycle}"
+            # Use non-blocking `Isend` for wear messages
             comm.isend(maintenance_message, dest=0, tag=maintenance_tag)  # Non-blocking send
             accumulated_wear = 0  # Reset wear after maintenance
 
         # Update current operation for the next cycle
-        current_operation = operation_sequence[(cycle + 1) % len(operation_sequence)]
-        current_operation = operations[current_operation_index]
-        cycle += 1
+        current_operation_index = (current_operation_index + 1) % len(operation_sequence)
+        initial_operation = operation_sequence[current_operation_index]
 
-        # Check for incoming maintenance messages before starting operations
-        while comm.Iprobe(source=MPI.ANY_SOURCE, tag=maintenance_tag):
-            maintenance_log = np.empty(log_size, dtype='S')
-            comm.Recv(maintenance_log, source=MPI.ANY_SOURCE, tag=maintenance_tag)
-            print(f"Machine {rank} received maintenance log: {maintenance_log.tostring().decode('utf-8')}")
-
-        # Update current operation for the next cycle
-        current_operation_index = (current_operation_index + 1) % len(operations)
-        current_operation = operations[current_operation_index]
-        cycle += 1
+    # Handle any remaining incoming maintenance messages
+    while comm.Iprobe(source=MPI.ANY_SOURCE, tag=maintenance_tag):
+        maintenance_log = np.empty(log_size, dtype='S')
+        request = comm.irecv(maintenance_log, source=MPI.ANY_SOURCE, tag=maintenance_tag)
+        request.Wait() # Wait for the non-blocking receive to complete
+        print(f"Machine {rank} received maintenance log: {maintenance_log.tostring().decode('utf-8')}")
 
 
-# Main control room logic for initializing and orchestrating the simulation
+# Control room logic for distributing initial data and collecting final product and maintenance logs
 def main_control_room(input_file, output_file, size):
     # Parse the input file to get initial settings
     num_cycles, threshold, wear_factors, parent_child_relations, machine_operations, initial_products = parse_input_file(input_file)
+
+    # Initialize a dictionary to store the data to be sent to each machine
+    machine_data = {}
+
+    # Distribute initial data to each machine
+    for machine_id in range(1, size):
+        parent, children = parent_child_relations.get(machine_id, (None, []))
+        initial_operation = machine_operations.get(machine_id, 'add')  # Default to 'add' if operation is not specified
+        initial_product = initial_products.get(machine_id, None)
+        machine_data[machine_id] = (parent, children, initial_operation, wear_factors, threshold, num_cycles, initial_product)
 
     # Use MPIPoolExecutor for distributing initial data
     with MPIPoolExecutor(max_workers=size) as executor:
         # Prepare and distribute data to each machine
         futures = []
-        for machine_id in range(1, size):
-            parent = parent_child_relations[machine_id]['parent']
-            children = parent_child_relations[machine_id].get('children', [])
-            initial_operation = machine_operations[machine_id]
-            initial_product = initial_products.get(machine_id, None)
-            data_to_send = (machine_id, parent, children, initial_operation, wear_factors, threshold, num_cycles, initial_product)
+        for machine_id, data_to_send in machine_data.items():
             future = executor.submit(comm.send, data_to_send, dest=machine_id)
             futures.append(future)
-        
+
         # Ensure all data is sent
         for future in futures:
             future.result()
 
-    # Assuming parent_child_relationships is predefined
-    # This should be a dictionary mapping machine_id to its (parent, children) tuple
-    parent_child_relationships = {}  # Initialize or load from configuration
-
-        # Distribute initial configuration to each machine
-    for machine_id in range(1, size):
-        parent, children = parent_child_relationships.get(machine_id, (None, []))
-        comm.send((parent, children, operations, wear_factors, threshold), dest=machine_id)
-
     # Collect final product and maintenance logs
-        final_product = None
-        maintenance_logs = []
-        for _ in range(size - 1):
-            status = MPI.Status()
-            data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
-            if status.tag == 99:
-                final_product = data
-            else:
-                maintenance_logs.append(data)
+    final_product = None
+    maintenance_logs = []
 
-    '''
-    # Master process logic for initial input distribution
-    with open(input_file, 'r') as f:
-        lines = f.readlines()
-        data_to_process = [line.strip() for line in lines]'''
+    # Receive final product and maintenance logs from each machine
+    for _ in range(size - 1):
+        status = MPI.Status()
+        data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+        if status.tag == 99:
+            final_product = data
+        else:
+            maintenance_logs.append(data)
 
     # Write the final product and maintenance logs to the output file
     with open(output_file, 'w') as f:
@@ -225,7 +264,6 @@ def main_control_room(input_file, output_file, size):
             f.write(f"Final product: {final_product}\n")
         for log in maintenance_logs:
             f.write(f"{log}\n")
-
 
 if __name__ == "__main__":
     if rank == 0:
@@ -240,8 +278,4 @@ if __name__ == "__main__":
         # Receive initial settings (blocking receive)
         parent, children, initial_operation, received_wear_factors, received_threshold, num_cycles, initial_product = comm.recv(source=0)
         machine_process(rank, parent, children, operations, received_wear_factors, received_threshold, num_cycles, initial_product)
-        initial_data = comm.recv(source=0)
-        machine_process(*initial_data)
 
-# Finalize MPI
-MPI.Finalize()
